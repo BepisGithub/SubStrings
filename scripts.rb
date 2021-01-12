4ae7808c3a768f29c07385450350833f64a473bd
bepis <luqdek@gmail.com>
2021-01-12 09:12:10 +0000

    Changed enumerable to a reduce method that fills the hash with the substrings and how many times they occured

--- scripts.rb
+++ scripts.rb
@@ -1,10 +1,20 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 def substrings(str,arr)
     str.downcase!
-    hash = arr.select do |word|
-        str.include?(word)
+    # hash = arr.select do |word|
+    #     str.include?(word)
+    # end
+
+    arr.reduce(Hash.new(0)) do |hash,word|
+        if str.include?(word)
+            hash[word] += 1
+            hash
+        else
+            hash
+        end
     end
-    p hash
+
+    
 
 end
 
