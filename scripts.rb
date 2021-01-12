5011fd2980826291221f5850d76a6ddc6abd7da9
bepis <luqdek@gmail.com>
2021-01-12 09:01:21 +0000

    Added enumerable method on the array to filter through the words that contain substrings

--- scripts.rb
+++ scripts.rb
@@ -1,4 +1,10 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 def substrings(str,arr)
+    hash = arr.select do |word|
+        str.include?(word)
+    end
+    p hash
 
 end
+
+substrings("Below the ocean",dictionary)
